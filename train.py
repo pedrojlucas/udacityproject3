1451a530a3037129341617dc65b71b5558f5aeee
Pedro Lucas <pedrojlucas@gmail.com>
2021-12-02 19:25:03 +0100

    One hot encoding and scaling

--- train.py
+++ train.py
@@ -5,47 +5,39 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder , MinMaxScaler
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
 def clean_data(data):
-    # Dict for cleaning data
-    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
-
+    
     # Clean and one hot encode data
-    x_df = data.to_pandas_dataframe().dropna()
-    jobs = pd.get_dummies(x_df.job, prefix="job")
-    x_df.drop("job", inplace=True, axis=1)
-    x_df = x_df.join(jobs)
-    x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
-    x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
-    contact = pd.get_dummies(x_df.contact, prefix="contact")
-    x_df.drop("contact", inplace=True, axis=1)
-    x_df = x_df.join(contact)
-    education = pd.get_dummies(x_df.education, prefix="education")
-    x_df.drop("education", inplace=True, axis=1)
-    x_df = x_df.join(education)
-    x_df["month"] = x_df.month.map(months)
-    x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
-    x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
-
-    y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    x_df = data
+    x_df['Age'] = (x_df['Age'] - np.min(x_df['Age'])) /(np.max(x_df['Age'])-np.min(x_df['Age']))
+    x_df['RestingBP'] = (x_df['RestingBP'] - np.min(x_df['RestingBP']))/(np.max(x_df['RestingBP'])-np.min(x_df['RestingBP']))
+    x_df['Cholesterol'] = (x_df['Cholesterol'] - np.min(x_df['Cholesterol']))/(np.max(x_df['Cholesterol'])-np.min(x_df['Cholesterol']))
+    x_df['MaxHR'] = (x_df['MaxHR'] - np.min(x_df['MaxHR']))/(np.max(x_df['MaxHR'])-np.min(x_df['MaxHR']))
+    x_df['Oldpeak'] = (x_df['Oldpeak'] - np.min(x_df['Oldpeak']))/(np.max(x_df['Oldpeak'])-np.min(x_df['Oldpeak']))
+    chestpain = pd.get_dummies(x_df['ChestPainType'], prefix="ChestPain")
+    x_df.drop("ChestPainType", inplace=True, axis=1)
+    x_df = x_df.join(chestpain)
+    x_df["Sex"] = x_df['Sex'].apply(lambda s: 1 if s == "M" else 0)
+    x_df["ExerciseAngina"] = x_df['ExerciseAngina'].apply(lambda s: 1 if s == "Y" else 0)
+    restingecg = pd.get_dummies(x_df['RestingECG'], prefix="RestingECG")
+    x_df.drop("RestingECG", inplace=True, axis=1)
+    x_df = x_df.join(restingecg)
+        
+    y_df = x_df.pop("HeartDisease")
 
     return x_df, y_df
     
 
 def main():
     
-    # Create TabularDataset using TabularDatasetFactory
-    # Data is located at:
-    # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+    # Create pandas dataframe from 'heart.csv' data file.
 
-    ds = TabularDatasetFactory.from_delimited_files('https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv')
+    ds = pd.read('./heart.csv')
 
     x, y = clean_data(ds)
 
