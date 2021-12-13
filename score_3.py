c74977ef7d5717e8f35e386b42617ee3ae88280e
Pedro Lucas <pedrojlucas@gmail.com>
2021-12-13 19:38:34 +0100

    updating score to have columns information

--- score_3.py
+++ score_3.py
@@ -3,6 +3,8 @@ import pandas as pd
 import joblib
 from azureml.core.model import Model
 
+cols = ['Age', 'Sex', 'ChestPainType', 'RestingBP', 'Cholesterol', 'FastingBS', 'RestingECG', 'MaxHR', 'ExerciseAngina', 'Oldpeak', 'ST_Slope']
+
 def init():
     global model
 
@@ -19,7 +21,7 @@ def run(data):
         # model_path = Model.get_model_path('my-udacityproj3-hyperdrivemodel')
         model_path = Model.get_model_path('my-udacityproj3-automlmodel')
         model = joblib.load(model_path)
-        data = pd.DataFrame(json.loads(data)['data'])
+        data = pd.DataFrame(json.loads(data)['data'], columns=cols)
         result = model.predict(data)
         return result.tolist()
     except Exception as e:
